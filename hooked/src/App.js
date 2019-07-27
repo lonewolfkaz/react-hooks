d83999da18d3b2878274d91aea5fd338a1d0be05
Anatoly <rook11@list.ru>
2019-07-27 15:06:05 +0300

    master: useEffect hook is done

--- hooked/src/App.js
+++ hooked/src/App.js
@@ -2,15 +2,42 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => setCount((prevCount) => {
-    return prevCount + 1;
-  });
+  const [city, setCity] = useState("")
+  const [country, setCountry] = useState("")
+
+  const handleCityChange = (event) => setCity(event.target.value);
+  const handleCountryChange = (event) => setCountry(event.target.value);
+
+  function handleReset() {
+    setCity("");
+    setCountry("");
+  }
 
   return (
     <div className="App">
-      <button onClick={handleClick}>Increase</button>
-      <h2>{count}</h2>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="City"
+            value={city}
+            onChange={handleCityChange}>
+          </input>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Country"
+            value={country}
+            onChange={handleCountryChange}>
+          </input>
+        </div>
+        <div>
+          <p>You live in {`${city}, ${country}`}</p>
+          <br />
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </form>
     </div>
   );
 }
